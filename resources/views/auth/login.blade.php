4fe7b7b4a8caf74ad8f7ea74ad6abbd5306281b1
Reifan Rachmanto <reifanrahmanto7@gmail.com>
2026-07-15 16:58:15 +0700

    force https

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/login.css') }}">
 </head>
 
 <body class="auth-modern">
