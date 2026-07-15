f3c2abd948ce748f59cb7900d8c28bb850764d45
Reifan Rachmanto <reifanrahmanto7@gmail.com>
2026-07-15 17:39:57 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Paksa semua URL menggunakan HTTPS saat production
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
